a3dac5408fb8aa2fce0c2113fe88efac03bc10d4
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2025-10-26 14:46:26 +0530

    Just prints the home directory

--- app.go
+++ app.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"os/user"
 )
 
 // App struct
@@ -24,4 +26,14 @@ func (a *App) startup(ctx context.Context) {
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func (a *App) GetHomeDir() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Printf("Can't get user's home directory")
+		return ""
+	}
+	return usr.HomeDir
+
 }
