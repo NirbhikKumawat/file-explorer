d34fb41edf6d6da7a31531f31ba6f393b6412500
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2025-10-27 19:22:19 +0530

    added comments to app.go

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 import logo from './assets/images/logo-universal.png';
 import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import {Greet} from "../wailsjs/go/main/App";// function imported from backend
 
 function App() {
     const [resultText, setResultText] = useState("Please enter your name below 👇");
@@ -10,13 +10,14 @@ function App() {
     const updateResultText = (result) => setResultText(result);
 
     function greet() {
-        Greet(name).then(updateResultText);
+        Greet(name).then(updateResultText);// calls Greet method defined in backend
+        // on resolving the promise the value in text box is updated
     }
 
     return (
         <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
+            <img src={logo} id="logo" alt="logo"/> {/*Image displayed*/}
+            <div id="result" className="result">{resultText}</div> {/*message telling to enter name*/}
             <div id="input" className="input-box">
                 <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
                 <button className="btn" onClick={greet}>Greet</button>
